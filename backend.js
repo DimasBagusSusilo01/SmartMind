907f7d4c0d85765983b0b383f834057426650b1c
DimasBagusSusilo01 <dimasbagussusilo01@gmail.com>
2026-08-09 15:15:06 +0100

    lagi

--- backend.js
+++ backend.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = "https://tlbctoielkmanpkzthwp.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://tlbctoielkmanpkzthwp.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRsYmN0b2llbGttYW5wa3p0aHdwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYyMTE0NTIsImV4cCI6MjEwMTc4NzQ1Mn0.9DsFYg6l5MF-Y6NRNGZIP8n_Axl_tV9Fjo1J4hBsYq0";
 
 import { createClient } from 'https://esm.sh/@supabase/supabase-js';
